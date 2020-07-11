3c5ad5a3820db69a846dbfcbaf1b715bda11b187
nim.yuan <nwyuan2011@gmail.com>
2020-07-12 00:44:01 +0200

    new class PrintSomEnVerschil added

---
+++ PrintSomEnVerschil.java
@@ -0,0 +1,11 @@
+// dag 1-2
+public class PrintSomEnVerschil {
+   public static void main(String[] args) {
+	  int x = 8;
+	  int y = 2;
+	  // print x + y
+	  System.out.println(x + y);   
+	  // print x - y
+	  System.out.println(x - y);
+   }
+}
